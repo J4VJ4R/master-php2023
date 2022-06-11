687bbc6cb35680a72127a86a06e2c02f6c2a8d79
unknown <javier.jaramillo@oohrd.com>
2022-06-10 22:19:26 -0500

    create a new form

--- 07-super_global/index.php
+++ 07-super_global/index.php
@@ -6,9 +6,9 @@
 
 
 echo '<h1>';
-echo $_SERVER['SERVER_ADDR']."<br>"; // give the ip server
-echo $_SERVER['SERVER_NAME']."<br>"; // give name's server
-echo $_SERVER['SERVER_SOFTWARE']."<br>"; // give server's software
-echo $_SERVER['HTTP_USER_AGENT']."<br>"; // give browser's name
-echo $_SERVER['REMOTE_ADDR']."<br>"; // give client's ip
+echo $_SERVER['SERVER_ADDR']."<br>"; // get the ip server
+echo $_SERVER['SERVER_NAME']."<br>"; // get name's server
+echo $_SERVER['SERVER_SOFTWARE']."<br>"; // get server's software
+echo $_SERVER['HTTP_USER_AGENT']."<br>"; // gett browser's name
+echo $_SERVER['REMOTE_ADDR']."<br>"; // get client's ip
 echo '</h1>';
