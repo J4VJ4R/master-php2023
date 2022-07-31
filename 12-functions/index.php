bd8f23be87b9366899ca1567c727e9c7b7c32666
unknown <javier.jaramillo@oohrd.com>
2022-07-30 20:03:55 -0500

    Was create css, html, file to learn more front-end

--- 12-functions/index.php
+++ 12-functions/index.php
@@ -1,12 +1,28 @@
 <?php
 
 
-
+//EJEMPLO 1
 function tablaNumber($number){
     echo "TABLE OF NUMBER $number <br>";
 
     for ($i = 0;$i < 10; $i++){
         $result = $number * $i;
-        echo "number $number x $i = $result<br>";
+        echo " $number x $i = $result<br>";
     }
+    echo "<hr>"; 
 }
+
+// if (isset($_GET['number'])){
+//     tablaNumber($_GET['number']);
+// }else{
+//     echo 'No existe tabla';
+// }
+
+// for($i = 0; $i < 10; $i++ ){
+//     tablaNumber($i);
+// }
+
+//EJEMPLO 2
+
+// function calculadora(number1, number2);
+tablaNumber(5);
