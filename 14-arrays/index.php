10444b35202c46948a35fadf3301a79ad7b0ce51
unknown <javier.jaramillo@oohrd.com>
2022-08-18 14:40:13 -0500

    was create a simple web site with include php

--- 14-arrays/index.php
+++ 14-arrays/index.php
@@ -2,3 +2,41 @@
 
 //Arrays
 
+$movie = "Batman";
+$movies = array('Batman', 'Spider man', 'El señor de los anillos');
+$singers = ['2pac', 'Shackira', 'Robie Williams'];
+$people = array(
+    'Name' => 'Javier',
+    'Last name' => 'Jaramillo'
+);
+
+echo $people['Name']."<br>";
+echo $movies[0];
+echo "<br>".$singers[0];
+
+//range with for
+
+echo "<h2> List of movies<h2>";
+echo "<ul>";
+for($i = 0; $i < count($movies); $i++){
+    echo "<li>".$movies[$i]."</li>";
+}
+echo "</ul>";
+
+//range with foreach
+echo "<h2>List of singers";
+
+echo "<ul>";
+foreach($singers as $singer){
+    echo "<li>".$singer."</li>";
+}
+echo "</ul>";
+
+//arrays multidimensionales
+
+$contacts = array(
+    array(
+        
+    ),
+);
+
