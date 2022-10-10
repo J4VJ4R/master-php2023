e1d339f7044ef80d0e8d0f0b88166101c99c43fb
unknown <javier.jaramillo@oohrd.com>
2022-10-09 20:31:02 -0500

    variables

--- 03-variables/index.php
+++ 03-variables/index.php
@@ -1,8 +1,20 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Variables en PHP</h2>
+    <?php
+    $string = "esto es un string";
+    $entero = 55;
+    $boolean = True;
+    $fecha = date('M');
 
-$paragraph[] = "greatings from paris";
-$paragraph[] = "greatings from paris";
-
-echo "******************************\n";
-echo "soy una variable de tipo $paragraph ";
-var_dump($paragraph);
+    echo $string. ' ' . $entero. ' '. $boolean. ' '. $fecha;
+    ?>
+</body>
+</html>
