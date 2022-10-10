ba4759d2f7427388efa8fcf632ffb22dc234f182
unknown <javier.jaramillo@oohrd.com>
2022-10-09 23:57:36 -0500

    incremento y decremento

--- 06-operators/index.php
+++ 06-operators/index.php
@@ -8,3 +8,18 @@ echo 'Resta: '.($num1 - $num2).'<br>';
 echo 'Multiplicación: '.($num1 * $num2).'<br>';
 echo 'División: '.($num1 / $num2).'<br>';
 echo 'Resto: '.($num1 % $num2).'<br>';
+
+//incremente y decremento
+
+$year = 2022;
+
+//incremento
+
+$year++;
+
+echo $year;
+
+//decremento
+
+$year--;
+echo '-'.$year;
