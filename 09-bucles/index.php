bcd09e160a58516244d2edcdbf6c54d3938a04a4
unknown <javier.jaramillo@oohrd.com>
2022-11-06 16:17:50 -0500

    Exersise Table of multiplication with php

--- 09-bucles/index.php
+++ 09-bucles/index.php
@@ -17,12 +17,17 @@ while($num <= 20){
 echo("<hr>");
 
 // Table of multiplication
-
+echo("<h1>Table of: ".$_GET['num1']."</h1>");
+$num1 = 0;
 if(isset($_GET['num1'])){
-    $num1 = $_GET['num1'];
-    echo($num1);
+    while($num1 <= 10){
+        $num2 = $_GET['num1'];
+        $result = $num1 * $num2;
+        echo("<h3>".$num2." x ". $num1. " = ".$result."</h3>");
+        $num1++;
+    }
 }else{
-    echo("Enter a number for url example: /?num1=11");
+    echo("Enter a number for url example: /?num1=1");
 }
 
 echo("<hr>");
