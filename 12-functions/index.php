cde1e153462968e5f6b038c9e3a4a09e00c1fe4a
unknown <javier.jaramillo@oohrd.com>
2022-11-07 10:04:19 -0500

    two or more parameters

--- 12-functions/index.php
+++ 12-functions/index.php
@@ -7,9 +7,36 @@ function table($value){
 }
 if(isset($_GET['num'])){
     for($i=0; $i<=10; $i++){
-        echo("<h3>Table of $i</h3>");
-        table($i);
+        // echo("<h3>Table of $i</h3>");
+        // table($i);
     }
 }else{
     echo("Enter a number");
 }
+
+//get more of two parameters
+function calculator($val1, $val2, $negrita = False){
+    if($negrita){
+        echo('<h1>');
+    }
+    $suma = $val1 + $val2;
+    $resta = $val1 - $val2;
+    $mult = $val1 * $val2;
+    $div = $val1 / $val2;
+    $rest = $val1 % $val2;
+
+    echo("suma = $suma <br/>");
+    echo("resta = $resta <br/>");
+    echo("mult = $mult <br/>");
+    echo("div = $div <br/>");
+    echo("rest = $rest <br/>");
+    echo("<hr/>");
+    if($negrita){
+        echo('</h1>');
+    }
+
+}
+
+calculator(5, 11, 'True');
+calculator(5, 12);
+calculator(5, 10);
