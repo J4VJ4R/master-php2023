30180bf96ecf98803e26bb9a37bae41c9aaff935
unknown <javier.jaramillo@oohrd.com>
2022-11-07 02:03:37 -0500

    exercise 5

--- 11-exercise/example5.php
+++ 11-exercise/example5.php
@@ -1,14 +1,12 @@
 <?php
 
-// show number of numbers between two numbers
 if(isset($_GET['num1']) && isset($_GET['num2'])){
-    $num1 = $_GET['num1'];
-    $num2 = $_GET['num2'];
+    $counter = $_GET['num1'];
+    $limit = $_GET['num2'];
+
+    for($counter; $counter<=$limit; $counter++){
+        echo('<h3>'.$counter.'</h3>');
+    }
 }else{
-    echo "Insert correct data on your url";
-}
-
-
-for($num1 = $num1 - 1; $num1 <= $num2; $num1++){
-    echo $num1."<br>";
+    echo("Enter the values on rul field");
 }
