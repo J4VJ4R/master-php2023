a093706c1e734445a0b10592511a6e127cecea17
unknown <javier.jaramillo@oohrd.com>
2022-11-07 09:30:14 -0500

    multiplication table with function

--- 12-functions/index.php
+++ 12-functions/index.php
@@ -1,28 +1,15 @@
 <?php
 
-
-//EJEMPLO 1
-function tablaNumber($number){
-    echo "TABLE OF NUMBER $number <br>";
-
-    for ($i = 0;$i < 10; $i++){
-        $result = $number * $i;
-        echo " $number x $i = $result<br>";
+function table($value){
+    for($i=0; $i<=10; $i++){
+        echo('<h3>'.$value.' * '. $i. ' = '.$value * $i);
     }
-    echo "<hr>"; 
 }
-
-// if (isset($_GET['number'])){
-//     tablaNumber($_GET['number']);
-// }else{
-//     echo 'No existe tabla';
-// }
-
-// for($i = 0; $i < 10; $i++ ){
-//     tablaNumber($i);
-// }
-
-//EJEMPLO 2
-
-// function calculadora(number1, number2);
-tablaNumber(5);
+if(isset($_GET['num'])){
+    for($i=0; $i<=10; $i++){
+        echo("<h3>Table of $i</h3>");
+        table($i);
+    }
+}else{
+    echo("Enter a number");
+}
