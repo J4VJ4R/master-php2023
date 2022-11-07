2ea3e73179fe685ddccd3ba640d449c64a88c6a8
unknown <javier.jaramillo@oohrd.com>
2022-11-07 01:21:48 -0500

    bucle for

--- 10-loop_for/index.php
+++ 10-loop_for/index.php
@@ -1,25 +1,7 @@
 <?php
 
-echo "<h1>BUCLE FOR</h1>";
-echo "<h1>EXAMPLE 1</h1>";
+//bucle for
 
-$sum = 0;
-for($num = 0; $num <= 10; $num++){
-    $sum += $num;
-    echo "<h3>$sum</h3>";
-}
-
-echo "The sum is: $sum";
-
-echo "<hr>";
-
-
-$num = (int)$_GET['number'];
-
-echo "<h1>EXAMPLE 2 - MULTIPLICATION TABLE OF $num</h1>";
-
-$result = 0;
-for($counter = 0; $counter <= 10; $counter++){
-    $result = $num * $counter;
-    echo "<h3> $num x $counter = $result</h3>";
+for($i=0; $i<=10; $i++){
+    echo('<h3>'.$i.'</h3>');
 }
