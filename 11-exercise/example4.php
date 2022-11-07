7ed7d8efc352a96c6e8731d955f867ee289a7583
unknown <javier.jaramillo@oohrd.com>
2022-11-07 01:51:53 -0500

    Please enter the commit message for your changes. Lines starting

--- 11-exercise/example4.php
+++ 11-exercise/example4.php
@@ -1,17 +1,25 @@
 <?php
 
-//get two numbers of url parameter
 
 
 if(isset($_GET['num1']) && isset($_GET['num2'])){
     $num1 = (int)$_GET['num1'];
     $num2 = (int)$_GET['num2'];
+    $suma = ($num1 + $num2);
+    $resta = $num1 - $num2;
+    $mult = $num1 * $num2;
+    $div = $num1 / $num2;
+    $resto = $num1 % $num2;
+    //suma
+    echo('<h3>'.$suma.'</h3>');
+    //resta
+    echo('<h3>'.$resta.'</h3>');
+    //multiplication
+    echo('<h3>'.$mult.'</h3>');
+    //division
+    echo('<h3>'.$div.'</h3>');
+    //resi
+    echo('<h3>'.$resto.'</h3>');
 }else{
-    echo "Please insert correct data for url";
+    echo("Enter the number on the url field");
 }
-
-echo "The sum of $num1 + $num2 is: " .($num1 + $num2)."<br>";
-echo "The rest of $num1 - $num2 is: " .($num1 - $num2)."<br>";
-echo "The multiplication of $num1 x $num2 is: " .$num1 * $num2."<br>";
-echo "The division of $num1 / $num2 is: " .$num1 / $num2."<br>";
-echo "The residue of $num1 % $num2 is: " .$num1 % $num2."<br>";
