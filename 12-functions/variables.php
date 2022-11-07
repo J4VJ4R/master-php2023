c04cf39d5aed3e1e596cc3dd33ef4296d556674a
unknown <javier.jaramillo@oohrd.com>
2022-11-07 12:26:58 -0500

    variables functions

--- 12-functions/variables.php
+++ 12-functions/variables.php
@@ -10,3 +10,30 @@ function name(){
 }
 
 echo(name());
+
+// variables functions
+echo("<hr/>");
+
+function goodMorning(){
+    return "<h2>good morning Javier</h2>";
+}
+
+function goodAfternoon(){
+    return'<h2>good afternoon Javier</h3>';
+}
+
+function goodEvening(){
+    return '<h3>good evening Javier</h3>';
+}
+if(isset($_GET['horario'])){
+    $horario = $_GET['horario'];
+    $saludo = "$horario";
+    echo($saludo());
+}else{
+    echo("enter horario on url field");
+}
+
+
+
+
+
