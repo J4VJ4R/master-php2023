254f89864111e607dc7a1ae40b47db6c11550935
unknown <javier.jaramillo@oohrd.com>
2022-11-07 03:01:16 -0500

    exercise 6

--- 11-exercise/example6.php
+++ 11-exercise/example6.php
@@ -1,19 +1,29 @@
-
-<?php
-
-// All multiplilcation tables in HTML tables
-
-$table1 = $_GET['table1'];
-$result = 0;
-echo "<table>";
-    echo "<tr>";
-        for($count = 0; $count <= 10; $count++) {
-            $result = $table1 * $count;
-            echo "<td>$result</td>";
-        }
-    echo "<tr>";
-echo "</table>";
-
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo("<table border='1'>");
+        echo("<tr>");
+            for($i=0; $i<=10; $i++){
+                    echo("<td>Tabla del: ".$i."</td>");
+                }
+        echo("</tr>");
+        for($j=0; $j<=10; $j++){
+            echo("<tr>");
+                for($i=0; $i<=10; $i++){
+                        echo("<td>".$i." * ".$j."  = ".($i * $j)."</td>");
+                    }
+            echo("</tr>");
+        }        
+        echo("</table>");
+    ?>
+</body>
+</html>
 
 
