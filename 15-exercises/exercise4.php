d0c0f77fffa2a2ca4338166a768f4038c589fab1
unknown <javier.jaramillo@oohrd.com>
2022-11-07 23:06:14 -0500

    exercise 4

---
+++ 15-exercises/exercise4.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$array = [1, 2, 3];
+$string = "string";
+$int = 5;
+$bool = False;
+
+echo getType($array).'<hr/>';
+echo getType($string).'<hr/>';
+echo getType($int).'<hr/>';
+echo getType($bool).'<hr/>';
+
