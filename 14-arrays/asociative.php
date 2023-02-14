de96a2cbde70f99602a32e79d84a9686c923829a
Javier <ingenierodesoftware73@gmail.com>
2023-02-14 17:16:14 -0500

    multidimensional arrays

--- 14-arrays/asociative.php
+++ 14-arrays/asociative.php
@@ -9,4 +9,35 @@ $user = [
 
 // var_dump($user["name"]);
 
-var_dump($_GET);
+// var_dump($_GET);
+
+
+// Multidimensional arrays
+$contacts = array(
+    array(
+        "name" => "Bereta",
+        "lastname" => "Barco",
+        "email" => "ner@Ams.com"
+    ),
+    array(
+        "name" => "Carlos",
+        "lastname" => "Castillo",
+        "email" => "car@Ams.com"
+    ),
+    array(
+        "name" => "Daniel",
+        "lastname" => "Daniels",
+        "email" => "daniel@Ams.com"
+    ),
+    array(
+        "name" => "America",
+        "lastname" => "Amstrong",
+        "email" => "Ame@Ams.com"
+    )
+);
+echo "<hr>";
+echo "<ul>";
+foreach ($contacts as $value) {
+    echo "<li>".$value["email"]."</li>";
+}
+echo "</ul>";
