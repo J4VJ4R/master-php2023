68deb9f5831e560636c1cbcbd71153a5b3183d75
Javier <ingenierodesoftware73@gmail.com>
2023-02-14 16:28:29 -0500

    methods of array

---
+++ 14-arrays/index.php
@@ -0,0 +1,10 @@
+<?php
+
+// Arrays first method
+$box = array('Orange', 'apple', 'strawberry');
+var_dump($box[0]);
+
+// Array second method
+echo "<hr>";
+$box2 = ['avocato', 'onion', 'banana'];
+var_dump($box2[1]);
