0e9842b4ca13f7910afd01bf23ce7473ab9a8ff2
Javier <ingenierodesoftware73@gmail.com>
2023-02-14 16:47:26 -0500

    for and foreach

--- 14-arrays/index.php
+++ 14-arrays/index.php
@@ -8,3 +8,22 @@ var_dump($box[0]);
 echo "<hr>";
 $box2 = ['avocato', 'onion', 'banana'];
 var_dump($box2[1]);
+
+// Iterate array with for
+echo "<hr>";
+echo "<ul>";
+for ($i=0; $i < count($box); $i++) { 
+    echo "<li>".$box[$i]."</li>";
+}
+echo "</ul>";
+
+// Iterate array with foreach
+echo "<hr>";
+echo "<ul>";
+foreach ($box2 as $key => $item) {
+    echo "<li>".$item."</li>";
+}
+
+echo "</ul>";
+echo "<hr>";
+
