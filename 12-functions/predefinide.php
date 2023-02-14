13d7e39a00c6977701ef07cbc0833c2a5aed3293
Javier <ingenierodesoftware73@gmail.com>
2023-02-13 23:23:00 -0500

    predefined variables

--- 12-functions/predefinide.php
+++ 12-functions/predefinide.php
@@ -1,65 +1,13 @@
 <?php
+//predefinidas
 
+echo "Random number between 10 and 20: ".rand(10, 20);
 
-// Debugging
-$name = 'javier';
+//date
+echo "<hr>";
+echo "The day is: ".date("D-M-y");
 
-var_dump($name);
+//round number
+echo "<hr>";
+echo "The round number of 4.789 is: ".round(4.789);
 
-// Date
-echo("<hr/>");
-
-echo(date('d-m-y'));
-
-// Timestamp
-echo("<hr/>");
-
-echo(time());
-
-// Maths
-echo("<hr/>");
-
-echo(sqrt(9));
-
-// Random number
-
-echo("<hr/>");
-echo(rand(1, 6));
-
-// pi
-
-echo("<hr/>");
-echo(pi());
-
-// round
-echo("<hr/>");
-
-echo(round(8.489209));
-
-//count characters
-echo("<hr/>");
-
-$name = "Javier";
-
-echo('the word contain '.strlen($name).' letters');
-
-//find word or character
-echo("<hr/>");
-
-$sentence = "the car w don't work";
-
-echo(strpos($sentence, 'w'));
-
-//change word
-
-echo("<hr/>");
-$sentence = str_replace('the', 'you', $sentence);
-echo $sentence;
-//change uppercase
-echo("<hr/>");
-
-echo strtoupper($sentence);
-//chage lowercase
-echo("<hr/>");
-
-echo strtolower($sentence);
