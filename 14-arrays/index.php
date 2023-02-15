8f842661b910d11c37d03275df9681b93fe8282e
Javier <ingenierodesoftware73@gmail.com>
2023-02-14 23:03:04 -0500

    function arrays

--- 14-arrays/index.php
+++ 14-arrays/index.php
@@ -27,3 +27,35 @@ foreach ($box2 as $key => $item) {
 echo "</ul>";
 echo "<hr>";
 
+// array functions
+$numbers = array("orange", "turtle", 5, 6, 9, "zaphiro");
+var_dump($numbers);
+echo "<hr>";
+
+// Add elements
+
+array_push($numbers, 55, 27);
+array_pop($numbers);
+sort($numbers);
+unset($numbers[6]);
+var_dump($numbers);
+echo "<hr>";
+
+// Random array
+$ran = array_rand($numbers);
+echo $numbers[$ran];
+echo "<hr>";
+
+// ordering on reverse
+var_dump(array_reverse($numbers));
+echo "<hr>";
+
+// Searching on array
+$search = array_search("orange", $numbers);
+var_dump($numbers[$search]);
+echo "<hr>";
+
+// Counting in an array
+$count = count($numbers);
+echo $count;
+echo "<hr>";
