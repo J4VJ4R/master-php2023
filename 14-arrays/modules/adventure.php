fde84ec1ae2ea9af63282e41af63514742de7a2d
Javier <ingenierodesoftware73@gmail.com>
2023-02-15 17:02:58 -0500

    exercise 5 arrays multidimensional

---
+++ 14-arrays/modules/adventure.php
@@ -0,0 +1,5 @@
+<tr>
+    <?php foreach ($games as $value) : ?>
+        <td><?= $value[2] ?></td>
+    <?php endforeach ?>    
+</tr>
---
+++ 14-arrays/modules/sports.php
@@ -0,0 +1,5 @@
+<tr>
+    <?php foreach ($games as $value) : ?>
+        <td><?= $value[0] ?></td>
+    <?php endforeach ?>    
+</tr>
