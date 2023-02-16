a72fee9f71d2f3da286a6ce3df73bc4ba725a82e
Javier <ingenierodesoftware73@gmail.com>
2023-02-16 13:22:12 -0500

    validation

---
+++ 18-forms/form4.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+
+        <label for="name">Nombre</label>
+        <input type="text" name="name" required pattern="[A-Za-z]+" id=""><br>
+        
+        <label for="lastname">Apellido</label>
+        <input type="text" name="lastname" id="" required pattern="[A-Za-z]+"><br>
+        
+        <label for="age">Edad</label>
+        <input type="text" name="age" required pattern="[0-9]+" id=""><br>
+        
+        <label for="email">Correo</label>
+        <input type="email" name="email" required  id=""><br>
+        
+        <label for="pass">Contraseña</label>
+        <input type="password" name="pass" required  id=""><br>
+        
+        <input type="submit" value="Send data">
+    </form>
+</body>
+</html>
