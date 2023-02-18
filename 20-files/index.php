1637aad46bd38d16338087b2943e81d7c5a8397b
Javier <ingenierodesoftware73@gmail.com>
2023-02-18 18:08:52 -0500

    copy, rename and delete file

--- 20-files/index.php
+++ 20-files/index.php
@@ -1,20 +1,30 @@
 <?php
 
-// Open
-$file = fopen("file.txt", 'a+');
+// // Open
+// $file = fopen("file.txt", 'a+');
 
-// Read
-while(!feof($file)){
-    $openfile = fgets($file);
-    echo $openfile."</br>";
-}
+// // Read
+// while(!feof($file)){
+//     $openfile = fgets($file);
+//     echo $openfile."</br>";
+// }
 
-// Set the pointer at the end of the file
-fseek($file, 0, SEEK_END);
-// Write
-fwrite($file, "This is a new text line sde test");
+// // Set the pointer at the end of the file
+// fseek($file, 0, SEEK_END);
+// // Write
+// fwrite($file, "This is a new text line sde test");
 
-//  Close
-fclose($file);
+// //  Close
+// fclose($file);
 
+// Copy file
+// copy('file.txt', 'copy_file.txt') or die("Error al copiar 2");
+
+// Rename
+// rename("copy_file.txt", "renamed_file.php");
+
+// Delete file
+unlink("renamed_file.php") or die("Error to delete file");
+
+?>
 
