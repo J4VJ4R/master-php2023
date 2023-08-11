4fc8bb2be6d76c97e70a993e59b9ce6b03f6f90d
javi <javier.jaramillo@oohrd.com>
2023-08-11 14:38:17 -0500

    operators

---
+++ 06-math-operators/index.php
@@ -0,0 +1,45 @@
+<?php
+
+
+$num1 = 55;
+$num2 = 100;
+
+echo 'Sum = '.$num1 + $num2.'<br>';
+echo 'Res = '.$num1 - $num2.'<br>';
+echo 'Mul = '.$num1 * $num2.'<br>';
+echo 'Div = '.$num1 / $num2.'<br>';
+echo 'Resi = '.$num1 % $num2.'<br>';
+
+echo '<hr>';
+
+// Incremento
+// $num++; = $num+1
+
+$num = 2021;
+$num++;
+echo $num;
+
+echo '<hr>';
+
+// Preincremento
+// ++$num = 1+$num
+
+$num = 2019;
+++$num;
+echo $num;
+echo '<hr>';
+
+// Drecremento
+// $num-- = $num-1
+
+$num = 2050;
+$num--;
+echo $num;
+echo '<hr>';
+
+// Predecremento
+// --$num = 1-$num;
+
+$num = 2080;
+--$num;
+echo $num;
