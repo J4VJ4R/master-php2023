11e7723baea1dda30836b59749abf02ebd0ea6a8
javi <javier.jaramillo@oohrd.com>
2023-08-11 10:36:09 -0500

    print

---
+++ 02-print/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Print on screen</title>
+</head>
+<body>
+    <h1>Master on PHP</h1>
+    <?php
+        echo '<ul>'
+            .'<h3>List of videogames</h3>'
+                .'<li>GTA</li>'
+                .'<li>Mario Bros</li>'
+                .'<li>FIFA</li>'
+            .'</ul>';
+    ?>
+</body>
+</html>
