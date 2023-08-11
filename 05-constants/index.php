7ae7a71e52a764eeb1b45e9be0a2df5dd3e00df9
javi <javier.jaramillo@oohrd.com>
2023-08-11 11:34:44 -0500

    constants

---
+++ 05-constants/index.php
@@ -0,0 +1,18 @@
+<?php
+
+define('name', 'Javier');
+define('charge', 'Senior developer');
+
+
+echo name.'<br>';
+echo charge;
+
+echo '<hr>';
+
+echo PHP_OS;
+echo '<hr>';
+echo 'PHP VERSION '.PHP_VERSION;
+echo '<hr>';
+echo PHP_EXTENSION_DIR;
+echo '<hr>';
+echo __FILE__;
