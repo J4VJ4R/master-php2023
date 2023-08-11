aec0ed2e63957d3f3a6168f3cd82eac73292e259
javi <javier.jaramillo@oohrd.com>
2023-08-11 14:46:07 -0500

    server variables

---
+++ 07-super_globals/index.php
@@ -0,0 +1,23 @@
+<?php
+
+// Super globals variables
+
+// server
+
+echo $_SERVER['SERVER_ADDR'];
+echo '<hr>';
+
+echo $_SERVER['SERVER_NAME'];
+echo '<hr>';
+
+
+echo $_SERVER['SERVER_SOFTWARE'];
+echo '<hr>';
+
+
+echo $_SERVER['HTTP_USER_AGENT'];
+echo '<hr>';
+
+echo $_SERVER['REMOTE_ADDR'];
+echo '<hr>';
+
