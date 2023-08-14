d142a07546d81f39dce4d6b2b4e39dc3d9ef92be
Javi <ingenierodesoftware73@gmail.com>
2023-08-14 18:56:33 -0500

    exercise 7

---
+++ 11-exercises/exercise_7.php
@@ -0,0 +1,19 @@
+<?php
+
+
+if (isset($_GET['number1']) && isset($_GET['number2'])) {
+    $n1 = $_GET['number1'];
+    $n2 = $_GET['number2'];
+    $start = $n1;
+    
+    for ($n1=$start; $n1 < $n2 ; $n1++) { 
+        if ($n1 !== $start) {
+            if ($n1 % 2 != 0) {
+                echo $n1;
+            }
+        }
+        $n1++;
+    }
+}else{
+    echo "Doesn't data enter by url, number1=x and number2=x";
+}
