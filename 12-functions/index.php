cb193ba837ddc8986b0ef350b530232b87ef60b1
Javi <ingenierodesoftware73@gmail.com>
2023-08-15 08:27:12 -0500

    Parameter false

--- 12-functions/index.php
+++ 12-functions/index.php
@@ -14,20 +14,26 @@
 // mul_table(5);
 
 
-function calc($value1, $value2){
+function calc($value1, $value2, $strong = false){
   $sum = $value1 + $value2;
   $rest = $value1 - $value2;
   $mul = $value1 * $value2;
   $div = $value1 / $value2;
   $resid = $value1 % $value2;
 
+  if($strong){
+    echo '<h1>';
+  }
   echo 'Sum: '.$sum.'<br>';
   echo 'Remains: '.$rest.'<br>';
   echo 'Multiplication: '.$mul.'<br>';
   echo 'Division: '.$div.'<br>';
+  if($strong){
+    echo '</h1>';
+  }
   echo 'Residue: '.$resid.'<hr>';
 }
 
 calc(5, 10);
-calc(5, 100);
+calc(5, 100, true);
 calc(55, 10);
