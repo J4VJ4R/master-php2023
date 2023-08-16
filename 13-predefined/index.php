15277b7e5b15d617791d178819300a23bb301410
Javi <javier.jaramillo@oohrd.com>
2023-08-16 11:20:06 -0500

    With javier.jaramillo

--- 13-predefined/index.php
+++ 13-predefined/index.php
@@ -1,24 +1,13 @@
 <?php
-// Establecer la zona horaria a America/Bogota
-date_default_timezone_set('America/Bogota');
+  require_once('includes/header.php');
+?>
+<!-- main -->
+<div>
+  <h1>Title</h1>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis fugiat facere, officiis id quo asperiores quis cum maxime molestias architecto cupiditate at? Unde,
+   nostrum veritatis adipisci odit consequatur tempora quam!</p>
+</div>
 
-// Días de la semana en español
-$dias_semana = array("Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado");
-
-// Meses en español
-$meses = array("", "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
-
-// Obtener el día de la semana, día del mes y mes actual
-$dia_semana = date("w"); // 0 (Domingo) a 6 (Sábado)
-$dia_mes = date("d");
-$mes_actual = date("n"); // 1 (Enero) a 12 (Diciembre)
-
-// Generar la fecha en formato español
-$fecha_espanol = $dias_semana[$dia_semana] . ", " . $dia_mes . " de " . $meses[$mes_actual] . " de " . date("Y");
-
-echo $fecha_espanol;
-
-
-echo '<hr>';
-echo time();
+<?php
+  require_once('includes/footer.php');
 ?>
