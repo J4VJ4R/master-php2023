c60633301748bd21a8937f7e61bce7531efbd7b4
Javier <ingenierodesoftware73@gmail.com>
2024-01-05 18:57:10 -0500

    Constants

--- 05-constants/index.php
+++ 05-constants/index.php
@@ -1,18 +1,5 @@
 <?php
 
-define('name', 'Javier');
-define('charge', 'Senior developer');
+define('name', 'Javier Jaramillo');
 
-
-echo name.'<br>';
-echo charge;
-
-echo '<hr>';
-
-echo PHP_OS;
-echo '<hr>';
-echo 'PHP VERSION '.PHP_VERSION;
-echo '<hr>';
-echo PHP_EXTENSION_DIR;
-echo '<hr>';
-echo __FILE__;
+echo name;
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>PHP course</h1>
+  <ol>
+    <li><a href="/01-home.php/">Proyecto Web</a></li>
+    <li><a href="/02-print/">Print</a></li>
+    <li><a href="/03-variables/">Variables</a></li>
+    <li><a href="/04-types/">Types</a></li>
+    <li><a href="/05-constants/">Constants</a></li>
+    <li><a href="/06-math-operators">Math Operators</a></li>
+    <li><a href="/07-super_globals">Super Globals</a></li>
+    <li><a href="/08-conditionals">Conditionals</a></li>
+    <li><a href="/09-bucles">Bucles</a></li>
+    <li><a href="/10-loop-for">Loop-for</a></li>
+    <li><a href="/11-exercises">Exercises</a></li>
+    <li><a href="/12-funciones">Functions</a></li>
+    <li><a href="/13-predefined">Predefined</a></li>
+  </ol>
+</body>
+</html>
