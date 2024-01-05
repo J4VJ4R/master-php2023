22194f5cfbdd9c620bf02236beccefdb184c273a
Javier <ingenierodesoftware73@gmail.com>
2024-01-05 02:48:03 -0500

    Update index

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>PHP course</h1>
+  <ol>
+    <li><a href="/01-hello-world/">Hello world</a></li>
+  </ol>
+</body>
+</html>
